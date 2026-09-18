52ea94db6590bcef4d696a43ef5000d03cb9b0e2
perebartrolisimo <pere.bartroli@ub.uu.se>
2026-09-18 09:54:09 +0000

    moving linking of data to last step

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -5,7 +5,7 @@ start(){
 	download_files "/tmp/clientInstallationFiles"
 	install_client
 	verify_installation
-	copy_keys_config_files
+	copy_and_apply_keys_config_files
 	run_backup
 }
 
@@ -31,7 +31,7 @@ download_files() {
 	    fi
 	done
     
-    echo "All files downloaded to: $output_dir"
+    echo "⇒ All files downloaded to: $output_dir"
 }
 
 install_client(){
@@ -44,12 +44,6 @@ install_client(){
       TIVsm-BAhdw.x86_64.rpm \
       --nodeps \
       --noposttrans
-
-	# Dynamic linker paths for TSM + GSKit
-	ldconfig || true
-
-	ln -sf /etc/tivoli/dsm.opt /opt/tivoli/tsm/client/ba/bin/dsm.opt
-	ln -sf /etc/tivoli/dsm.sys /opt/tivoli/tsm/client/ba/bin/dsm.sys
 }
 
 verify_installation(){
@@ -77,17 +71,25 @@ verify_installation(){
 		return 1
 	fi
 
-	echo "All packages verified as installed."
+	echo "⇒ All packages verified as installed."
 }
 
-copy_keys_config_files(){
+copy_and_apply_keys_config_files(){
+	echo "⇒ Copying config files and keys"
 	cp -a /tmp/opt/tivoli/tsm/client/ba/bin/. /opt/tivoli/tsm/client/ba/bin/
 	cp -a /tmp/etc/tivoli/. /etc/tivoli/
 	cp -a /tmp/etc/ld.so.conf.d/. /etc/ld.so.conf.d
+	
+	echo "⇒ Apply config files"
+	# Dynamic linker paths for TSM + GSKit
+	ldconfig || true
+
+	ln -sf /etc/tivoli/dsm.opt /opt/tivoli/tsm/client/ba/bin/dsm.opt
+	ln -sf /etc/tivoli/dsm.sys /opt/tivoli/tsm/client/ba/bin/dsm.sys
 }
 
 run_backup(){
-	echo "Start backup"
+	echo "⇒ Start backup"
 	echo "TMP: starting with a nap! zZzzZzzZzzZz "
 	sleep inf
 }
